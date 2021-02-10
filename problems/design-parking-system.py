b14b04cd07dea11e9e91b21fccadec31d9a2724e
costalauro <costalauro@hotmail.com>
2021-02-10 19:40:24 -0300

    design parking system

--- problems/design-parking-system.py
+++ problems/design-parking-system.py
@@ -1,4 +1,7 @@
-# Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+#1603. Design Parking System
+
+# Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small,
+# with a fixed number of slots for each size.
 
 # Implement the ParkingSystem class:
 
