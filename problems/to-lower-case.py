7004fc7354b6549ae4facc664338c43ea8ec0bca
costalauro <costalauro@hotmail.com>
2021-02-12 20:19:08 -0300

    adicao de novos resolucoes do problema to lower case utilizando unicode

--- problems/to-lower-case.py
+++ problems/to-lower-case.py
@@ -22,4 +22,32 @@ def to_lower_case(str: str) -> str:
     return str.lower()
 
 
-print(to_lower_case("Hello"))
+# print(to_lower_case("Hello"))
+
+def to_lower_case_verbose(str: str) -> str:
+    l1 = []  # list to store lower case char
+
+    for c in str:
+        if "A" <= c <= "Z":  # check whether the char is uppercase
+            unicode = ord(c) + 32  # add 32 unicode value to it
+            char = chr(unicode)  # convert into Char
+            l1.append(char)
+        else:
+            l1.append(c)  # add to list as it is in lowercase
+
+    return ("".join(l1))
+
+
+print(to_lower_case_verbose("Hello"))
+
+
+def to_lower_case_using_unicode_one_line(str):
+    return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in str)
+    # return "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in str)
+
+
+print(to_lower_case_using_unicode_one_line("Hello"))
+
+
+
+
